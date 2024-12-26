b27830f6cb621d021d2dbd33b4baf6dc85629149
unknown <nahidujjamanraj@gmail.com>
2024-12-26 18:25:09 +0600

    In this program I know about limitation of card and set show all button

--- JS/w1.js
+++ JS/w1.js
@@ -1,16 +1,30 @@
-const phone_api=async ()=>{
-    const phone_fetch=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const phone_api=async(input_text)=>{
+    const phone_fetch=await fetch(`https://openapi.programming-hero.com/api/phones?search=${input_text}`)
     const phone_json =await phone_fetch.json();
     const phone_data=phone_json.data
     phone_api_details(phone_data)
 }
 
 const phone_api_details=(data_s)=>{
+  // find the element 
     let find_item=document.getElementById('phone_container')
+    find_item.innerHTML=''
+    console.log(data_s.length)
+    let find_button=document.getElementById('show_all')
+    if(data_s.length >12){
+      find_button.classList.remove('hidden')
+    }else if(data_s.length<12){
+      find_button.classList.add('hidden')
+    }
+    // limitation
+    data_s=data_s.slice(0,10)
+    // run loop 
     for(let data of data_s){
         console.log(data)
+        // create dib
         let crete_div=document.createElement('div')
-        crete_div.classList=`card bg-base-100 w-96 shadow-xl`
+        crete_div.classList=`card bg-base-100 p-4 shadow-xl`
+        // set inner html
         crete_div.innerHTML=`
         <figure class="px-10 pt-10">
               <img
@@ -28,9 +42,28 @@ const phone_api_details=(data_s)=>{
               <div class="card-actions">
                 <button class="btn btn-primary">Show Details</button>
         `
+        // append child 
         find_item.appendChild(crete_div)
     }
 
 
 }
-phone_api()
+// search phone section 
+function search_phone(){
+  // find the id 
+  const find_phone=document.getElementById('input_text')
+  // find the man
+  let input_Value=find_phone.value;
+  console.log(input_Value)
+  // call function 
+  phone_api(input_Value)
+
+}
+// phone_api()
+
+function search_phone2(){
+  const find_search_phone2=document.getElementById('input_text2')
+  const find_search_phone2_man=find_search_phone2.value;
+  phone_api(find_search_phone2_man)
+
+}
